c7fab8c07778d48e5926ae2a0a84c1d06bf129bd
NHHLong <nguyenlong230905@gmail.com>
2026-04-08 04:03:26 +0700

    fix home

--- src/routes/home.route.js
+++ src/routes/home.route.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const rooms = require('../data/mockData').adminRooms;
 const homeController = require('../controllers/home.controller');
 
-router.get('/', homeController.home);
+//router.get('/', homeController.home);
 
 router.get('/profile', homeController.getProfile);
 router.post('/profile/update', homeController.updateProfile);
